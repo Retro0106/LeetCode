0257be66225b76ffc18305e8fdac15f4e729472a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-12-01 18:13:18 -0600

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        hashSet = set()
+       
+        if len(s) < 1:
+            return 0
+        longest = 1
+        i = 0
+        for j in range(len(s)):
+            while s[j] in hashSet:
+                longest = max(longest, j-i)
+                hashSet.remove(s[i])
+                i += 1
+            hashSet.add(s[j])
+        longest = max(longest, j-i)
+        return longest
