56f8ad327e344d17901cf851db00428ebbdf9e97
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-12-05 13:45:01 -0600

    LC-top-k-frequent-elements [Runtime: 5 ms; Memory: 14.2 MB]

---
+++ bucket-sort/top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        output = []
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        
+        rank = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+        for i in range(k):
+            output.append(rank[i][0])
+        return output
+
+
