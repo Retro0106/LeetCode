847178e0b6563ad39b284194eac0c6e7b24d342b
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-12-06 00:53:31 -0600

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        minimum = 0
+        minimum = float('-inf')
         maxProfit = 0
         for num in prices:
             minimum = min(minimum, num)
