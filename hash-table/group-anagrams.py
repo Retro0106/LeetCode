426f6bf3443599280d1f9d12b3bbfc95cf4ff1a8
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-12-05 19:33:10 -0600

    LC-group-anagrams [Runtime: 24 ms; Memory: 17.9 MB]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -5,24 +5,34 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        # hashmap = {}
+        # for char in strs:
+        #     hashmap[char] = Counter(char)
+        
+        # res = []
+        # my_set = set()
+
+        # for i in range(len(strs)):
+        #     if strs[i] not in my_set:
+        #         curr = []
+        #         my_set.add(strs[i])
+        #         curr.append(strs[i])
+                
+        #         for j in range(len(strs)):
+        #             if i != j and hashmap[strs[i]] == hashmap[strs[j]]:
+        #                 curr.append(strs[j])
+        #                 my_set.add(strs[j])
+        #         res.append(list(curr))
+        # return res
+
         hashmap = {}
         for char in strs:
-            hashmap[char] = Counter(char)
-        
-        res = []
-        my_set = set()
-
-        for i in range(len(strs)):
-            if strs[i] not in my_set:
-                curr = []
-                my_set.add(strs[i])
-                curr.append(strs[i])
-                
-                for j in range(len(strs)):
-                    if i != j and hashmap[strs[i]] == hashmap[strs[j]]:
-                        curr.append(strs[j])
-                        my_set.add(strs[j])
-                res.append(list(curr))
-        return res
-
-            
+            count = [0] * 26
+            for letter in char:
+                count[ord(letter)-ord('a')] += 1
+            key = tuple(count)
+            if key in hashmap:
+                hashmap[key].append(char)
+            else:
+                hashmap[key] = [char]
+        return hashmap.values()
