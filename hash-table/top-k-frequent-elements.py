bc1ebc01ea007cfd89ba28d1a7690387dc99950a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2024-12-06 01:03:48 -0600

    LC-top-k-frequent-elements [Runtime: 8 ms; Memory: 13.9 MB]

--- hash-table/top-k-frequent-elements.py
+++ hash-table/top-k-frequent-elements.py
@@ -5,17 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        hashmap = {}
-        output = []
-        for num in nums:
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
+        # hashmap = {}
+        # output = []
+        # for num in nums:
+        #     if num in hashmap:
+        #         hashmap[num] += 1
+        #     else:
+        #         hashmap[num] = 1
         
+        # rank = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+        # for i in range(k):
+        #     output.append(rank[i][0])
+        # return output
+
+        hashmap = {}
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
         rank = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
+        output = []
         for i in range(k):
             output.append(rank[i][0])
         return output
-
-
