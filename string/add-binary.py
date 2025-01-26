2b4b32b5c65784e219e4c490b30783fc5a42d411
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-26 02:13:28 -0600

    LC-add-binary [Runtime: N/A; Memory: N/A]

---
+++ string/add-binary.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        def decimal(string):
+            j = 0
+            total = 0
+            for i in range(len(string)-1, -1, -1):
+                total += int(string[i]) * (2 ** j)
+                j += 1
+            return total
+        
+        def binary(num):
+            number = ''
+            while num != 1:
+                if num % 2 == 0:
+                    number = '0' + number
+                else:
+                    number = '1' + number
+                num = num // 2
+            number = '1' + number
+            return number
+        
+        return binary(decimal(a) + decimal(b))
+
+
+        
