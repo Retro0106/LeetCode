8d79a8ddf1336f380716c2a79383f7c996f59bae
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-26 01:57:25 -0600

    LC-plus-one [Runtime: 0 ms; Memory: 12.5 MB]

--- math/plus-one.py
+++ math/plus-one.py
@@ -4,13 +4,21 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        number = ''
-        for num in digits:
-            number += str(num)
-        number = int(number)
+        # number = ''
+        # for num in digits:
+        #     number += str(num)
+        # number = int(number)
 
-        number += 1
-        res = []
-        for num in str(number):
-            res.append(int(num))
-        return res
+        # number += 1
+        # res = []
+        # for num in str(number):
+        #     res.append(int(num))
+        # return res
+
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
