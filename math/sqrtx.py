9f0a1b295c89eb2ea3d596c5bcf11707de71cea0
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-27 19:20:02 -0600

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.py
+++ math/sqrtx.py
@@ -5,10 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
-        if x == 0:
-            return 0
+        # if x == 0:
+        #     return 0
 
-        for i in range(x+1):
+        # for i in range(x+1):
+        #     if i * i > x:
+        #         return i-1
+        # return x
+
+        half = x//2
+        for i in range(half+1):
             if i * i > x:
                 return i-1
+            elif i*i == x:
+                return i
         return x
+
