b842831539da4d7086cbd1ec6cb459dd91a32488
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-27 20:41:10 -0600

    LC-sqrtx [Runtime: 8 ms; Memory: 12.4 MB]

--- math/sqrtx.py
+++ math/sqrtx.py
@@ -38,25 +38,41 @@ class Solution(object):
         mid = (left + right) // 2
         temp = False
 
-        while left < right:
-            mid = (left + right) // 2
+        # while left < right:
+        #     mid = (left + right) // 2
 
-            if temp and mid == temp:
-                if (mid+1) * (mid+1) > x:
-                    return mid 
-                return mid + 1
+        #     if temp and mid == temp:
+        #         if (mid+1) * (mid+1) > x:
+        #             return mid 
+        #         return mid + 1
             
-            if mid * mid == x:
+        #     if mid * mid == x:
+        #         return mid
+            
+        #     elif mid * mid < x:
+        #         left = mid
+            
+        #     elif mid * mid > x:
+        #         right = mid - 1 
+            
+        #     temp = mid
+        # return left
+            
+
+        if x < 2:  # Special case for 0 and 1
+            return x
+
+        left, right = 1, x // 2  # Binary search range: from 1 to x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:  # Exact square root found
                 return mid
-            
-            elif mid * mid < x:
-                left = mid
-            
-            elif mid * mid > x:
-                right = mid - 1 
-            
-            temp = mid
-        return left
-            
+            elif mid * mid < x:  # Move right if mid^2 is less than x
+                left = mid + 1
+            else:  # Move left if mid^2 is greater than x
+                right = mid - 1
+
+        # At this point, `right` is the largest integer such that `right^2 <= x`
+        return right
 
 
