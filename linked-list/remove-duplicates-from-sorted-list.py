de386d05217d3c950a99f17d2b4af88e9f8b8916
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-27 21:00:40 -0600

    LC-remove-duplicates-from-sorted-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        hashSet = set()
+        ptr = head
+        while ptr:
+            if ptr.val in hashSet:
+                temp.next = ptr.next
+            
+            hashSet.add(ptr.val)
+            temp = ptr
+            ptr = ptr.next
+        
+        return head
