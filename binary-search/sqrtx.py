6d97441b60e8bc6662ac202a04419b19c220ebdb
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-27 19:14:54 -0600

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- binary-search/sqrtx.py
+++ binary-search/sqrtx.py
@@ -5,6 +5,10 @@ class Solution(object):
         :rtype: int
         """
         
+        if x == 0:
+            return 0
+
         for i in range(x+1):
             if i * i > x:
                 return i-1
+        return x
