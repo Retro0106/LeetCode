4994e4d660ce757b57500fa61ec1a34bc27054f5
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-01-28 19:26:26 -0600

    LC-single-number [Runtime: 2 ms; Memory: 14 MB]

--- bit-manipulation/single-number.py
+++ bit-manipulation/single-number.py
@@ -4,12 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hashmap = {}
+        # METHOD I
+        result = 0
         for num in nums:
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
-        for key in hashmap:
-            if hashmap[key] == 1:
-                return key
+            result ^= num
+        return result
+
+        # METHOD II: O(n), o(n)
+        # hashmap = {}
+        # for num in nums:
+        #     if num in hashmap:
+        #         hashmap[num] += 1
+        #     else:
+        #         hashmap[num] = 1
+        # for key in hashmap:
+        #     if hashmap[key] == 1:
+        #         return key
+
+        # METHOD III: O(n^2), O(1)
+        # for x in nums:
+        #     if nums.count(x) == 1:
+        #         return x
