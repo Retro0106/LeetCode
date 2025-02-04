3d982c730d6c0c98bb3ab3ff5e55bb8d11168bb1
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-02-03 19:38:18 -0600

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.py
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,15 +7,47 @@ class Solution(object):
         """
         
           
+        # if target not in nums:
+        #     return [-1,-1]
+        # else:
+        #     left = nums.index(target)
+        #     right = 0
+        #     for i in range(len(nums)):
+        #         if nums[i] == target:
+        #             right = i
+        # return [left, right]
+
         if target not in nums:
-            return [-1,-1]
-        else:
-            left = nums.index(target)
-            right = 0
-            for i in range(len(nums)):
-                if nums[i] == target:
-                    right = i
-        return [left, right]
-
-
+            return [-1, -1]
+         
+        def searchLeft(nums):
+            left = 0
+            right = len(nums) - 1
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    right = mid
+                    if right != 0 and nums[right - 1] != target:
+                        return right
+                elif mid < target:
+                    left = mid + 1
+                elif mid > target:
+                    right = mid - 1
             
+
+
+        def searchRight(nums):
+            left = 0
+            right = len(nums) - 1
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    left = mid
+                    if left != 0 and nums[left + 1] != target:
+                        return left
+                elif nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
+        return [searchLeft(nums), searchRight(nums)]
+
