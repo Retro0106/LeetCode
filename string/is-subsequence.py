47df45d0e899e7d58bd72cb11455dd3149612047
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-12 13:45:08 -0500

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

--- string/is-subsequence.py
+++ string/is-subsequence.py
@@ -6,4 +6,6 @@ class Solution:
         for j in range(len(t)):
             if t[j] == s[i]:
                 i+=1
+            if i == len(s):
+                return True
         return i == len(s)
