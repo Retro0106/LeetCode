961761d403e4d6e8a3bf30c25da7c713d1a24a41
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-12 14:30:51 -0500

    LC-longest-common-prefix [Runtime: 0 ms; Memory: 17.9 MB]

--- string/longest-common-prefix.py
+++ string/longest-common-prefix.py
@@ -1,37 +1,11 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        string = ""
         strs.sort()
-        left = strs[0]
-        right = strs[len(strs)-1]
-        res = ''
-        for i in range(len(left)):
-            if left[i] != right[i]:
-                return res
-            res += left[i]
-        return strs[0]
+        for i in range(len(strs[0])):
+            if strs[0][i] != strs[-1][i]:
+                return string
+            string += strs[0][i]
+        return string
 
 
-
-
-        # i = 0
-        # j = 0
-        # k = 1
-        # new = ''
-        # if len(strs) == 1:
-        #     new = new + strs[0]
-        #     return new
-        # while len(strs) > 1 and i < len(strs)and j < len(min(strs)):
-        #     while k<len(strs):
-        #         if strs[0][j] != strs[k][j]:
-        #             return new
-        #         else:
-        #             k+=1
-        #     new+=strs[0][j]
-        #     j+=1 
-        #     k=1
-        # return new
-        
-                    
-                
-
-        
