0a8c45c06a1a902a4c83cd1a0a18d5792a66ab3c
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-12 14:13:04 -0500

    LC-best-time-to-buy-and-sell-stock [Runtime: 131 ms; Memory: 27 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -1,60 +1,9 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        minimum = float('inf')
-        maxProfit = 0
-        for num in prices:
-            minimum = min(minimum, num)
-            profit = num - minimum
-            maxProfit = max(maxProfit, profit)
-        return maxProfit
-        
-        
-        # minimum = float('inf')        
-        # max_profit = 0
-        # for price in prices:
-        #     minimum = min(minimum,price)
-            
-        #     profit = price - minimum
-        #     max_profit = max(profit,max_profit)
-        # return max_profit
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if len(prices)<2:
-        #     return 0
-        # maxProfit = []
-        # for i in range(len(prices)-1):
-        #     if i+1 < len(prices)-1:
-        #         if i <= max(prices[i+1:]):
-        #             profit = max(prices[i+1:]) - prices[i]
-        #             maxProfit.append(profit)
-        # if max(maxProfit)<=0:
-        #     return 0
-        # return max(maxProfit)
-
-
-
-
-        # for i in range(len(prices)):
-        #     for j in range(i+1, len(prices)):
-        #         profit = prices[j] - prices[i]
-        #         maxProfit.append(profit)
-        # if max(maxProfit)<=0:
-        #     return 0
-        # return max(maxProfit)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lowest = prices[0]
+        max_profit = 0
+        for price in prices:
+            profit = price - lowest
+            max_profit = max(max_profit, profit)
+            lowest = min(lowest, price)
+        return max_profit
