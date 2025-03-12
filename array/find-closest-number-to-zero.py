551199ef8c0fde2116c94507438ea23e11e2a74b
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-12 12:18:13 -0500

    LC-find-closest-number-to-zero [Runtime: 13 ms; Memory: 18 MB]

--- array/find-closest-number-to-zero.py
+++ array/find-closest-number-to-zero.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def findClosestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        smallest = nums[0]
-        for i in nums:
-            if abs(i) < abs(smallest):
-                smallest = i
-            elif abs(i) == abs(smallest):
-                smallest = max(i, smallest)
-        return smallest
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        hashmap = {}
+        for num in nums:
+            key = abs(num)
+            if key in hashmap:
+                hashmap[key] = max(hashmap[key], num)
+            else:
+                hashmap[key] = num
+        return hashmap[min(hashmap.keys())]
