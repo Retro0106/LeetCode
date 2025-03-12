1fa2b7a61c06f230589e5787fd77eb527d6a562f
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-12 12:27:33 -0500

    LC-find-closest-number-to-zero [Runtime: N/A; Memory: N/A]

--- array/find-closest-number-to-zero.py
+++ array/find-closest-number-to-zero.py
@@ -1,10 +1,20 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        hashmap = {}
-        for num in nums:
-            key = abs(num)
-            if key in hashmap:
-                hashmap[key] = max(hashmap[key], num)
-            else:
-                hashmap[key] = num
-        return hashmap[min(hashmap.keys())]
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[0] >= 0:
+                return nums[i]
+            elif nums[i] <= 0:
+                continue
+            if nums[i] >= 0 and nums[i-1] <= 0:
+                return nums[i]
+        return nums[-1]
+    
+        # hashmap = {}
+        # for num in nums:
+        #     key = abs(num)
+        #     if key in hashmap:
+        #         hashmap[key] = max(hashmap[key], num)
+        #     else:
+        #         hashmap[key] = num
+        # return hashmap[min(hashmap.keys())]
