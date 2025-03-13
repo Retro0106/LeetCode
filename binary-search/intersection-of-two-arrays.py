a97546d457370feb74b10dc114f47a29b2453f4a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 14:14:20 -0500

    LC-intersection-of-two-arrays [Runtime: 4 ms; Memory: 17.7 MB]

--- binary-search/intersection-of-two-arrays.py
+++ binary-search/intersection-of-two-arrays.py
@@ -1,3 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1).intersection(set(nums2)))
+        # return list(set(nums1).intersection(set(nums2)))
+        
+        output = []
+        hset = set(nums2)
+        for num in set(nums1):
+            if num in hset:
+                output.append(num)
+        return output
