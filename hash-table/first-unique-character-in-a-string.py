511ac12aff4175bd5402f902a9171c45323e22f6
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 13:28:14 -0500

    LC-first-unique-character-in-a-string [Runtime: 47 ms; Memory: 17.9 MB]

--- hash-table/first-unique-character-in-a-string.py
+++ hash-table/first-unique-character-in-a-string.py
@@ -1,17 +1,21 @@
-from collections import deque
+from collections import deque, Counter
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        q = deque()
-        hashmap = {}
+        # q = deque()
+        # hashmap = {}
         
-        for i in range(len(s)):
-            if s[i] not in hashmap:
-                q.append(s[i])
-                hashmap[s[i]] = i
-            else:
-                if s[i] in q:
-                    q.remove(s[i])
+        # for i in range(len(s)):
+        #     if s[i] not in hashmap:
+        #         q.append(s[i])
+        #         hashmap[s[i]] = i
+        #     else:
+        #         if s[i] in q:
+        #             q.remove(s[i])
                 
-        return hashmap[q[0]] if q else -1
+        # return hashmap[q[0]] if q else -1
             
-        
+        freq = Counter(s)
+        for i, j in enumerate(s):
+            if freq[j] == 1:
+                return i
+        return -1
