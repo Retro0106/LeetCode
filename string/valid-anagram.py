8b7025236f2deab47b1d91422c4970ab86f20e47
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 11:08:48 -0500

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,6 +1,14 @@
-from collections import Counter
+from collections import Counter, defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_Counter = Counter(s)
-        t_Counter = Counter(t)
-        return s_Counter == t_Counter
+        # s_Counter = Counter(s)
+        # t_Counter = Counter(t)
+        # return s_Counter == t_Counter
+        hashmap = defaultdict(int)
+        for char in s:
+            hashmap[char] += 1
+        for char in t:
+            hashmap[char] -= 1
+            if hashmap[char] == -1:
+                return False
+        return True
