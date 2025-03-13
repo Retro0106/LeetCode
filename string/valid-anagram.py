cffe63a99c41f320d22125cfae89b625daeb130d
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 11:05:06 -0500

    LC-valid-anagram [Runtime: 6 ms; Memory: 18 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,30 +1,6 @@
 from collections import Counter
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # return Counter(s) == Counter(t)
-
-        # hashmap1 = {}
-        # hashmap2 = {}
-        # for i in s:
-        #     hashmap1[i] = hashmap1.get(i, 0) + 1
-        # for i in t:
-        #     hashmap2[i] = hashmap2.get(i, 0) + 1
-        # return hashmap1 == hashmap2
-
-        hashmap = {}
-        for i in s:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        
-        for i in t:
-            if i not in hashmap:
-                return False
-            
-            hashmap[i] -= 1
-            if hashmap[i] == 0:
-                del hashmap[i]
-        return len(hashmap) == 0
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_Counter = Counter(s)
+        t_Counter = Counter(t)
+        return s_Counter == t_Counter
