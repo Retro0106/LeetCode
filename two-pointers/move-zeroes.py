32e80d258d5b269b8a32296595603578da10eed5
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 02:08:27 -0500

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- two-pointers/move-zeroes.py
+++ two-pointers/move-zeroes.py
@@ -1,21 +1,31 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-         
-        i = j = 0
-        while i < len(nums)-1:
-            if nums[i] == 0:
-                j = i+1
-                while j < len(nums):
-                    if nums[j] != 0:
-                        nums[i],nums[j] = nums[j], nums[i]
+        i,j = 0,0
+        for j in range(len(nums)):
+            while nums[i] != 0:
+                if i == len(nums) - 1:
+                    break
+                i += 1
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                while nums[i] != 0:
+                    if i == len(nums) - 1:
                         break
-                    j += 1
-            i += 1
+                    i += 1
         
 
-
         
+        # Doesnt keep the order intact
+
+        # i = 0
+        # j = len(nums)-1
+        # while nums[j] == 0 and j>=0:
+        #     j-=1
+        # while i < j:
+        #     if nums[i] == 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         j -= 1
+        #     i+=1
