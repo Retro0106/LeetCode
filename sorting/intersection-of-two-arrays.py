b14fd2d41335bbb383af04c110fa54c39fee4c4a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 14:17:24 -0500

    LC-intersection-of-two-arrays [Runtime: 0 ms; Memory: 17.7 MB]

--- sorting/intersection-of-two-arrays.py
+++ sorting/intersection-of-two-arrays.py
@@ -1,10 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1).intersection(set(nums2)))
+        # return list(set(nums1).intersection(set(nums2)))
         
-        output = []
+        output = set()
         hset = set(nums2)
         for num in set(nums1):
             if num in hset:
-                output.append(num)
-        return output
+                output.add(num)
+        return list(output)
