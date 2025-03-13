843e722a24ad453fe79aafc50b1438773501fc8b
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-13 13:55:53 -0500

    LC-contains-duplicate [Runtime: 4 ms; Memory: 31.5 MB]

--- hash-table/contains-duplicate.py
+++ hash-table/contains-duplicate.py
@@ -1,17 +1,3 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        
-        hashmap = {}
-        for i in nums:
-            if i in hashmap:
-                hashmap[i]+=1
-            else:
-                hashmap[i]=1
-        for i, j in hashmap.items():
-            if j >= 2:
-                return True
-        return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) > len(set(nums))
