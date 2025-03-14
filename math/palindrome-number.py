2e9de79ccacdd5d832148bdfc2c15694017a472d
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-14 16:45:51 -0500

    LC-palindrome-number [Runtime: 3 ms; Memory: 17.6 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,14 +1,4 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        # x = str(x)
-        # i = 0
-        # j = len(x)-1
-        # while i < j:
-        #     if x[i] != x[j]:
-        #         return False
-        #     i+=1
-        #     j-=1
-        # return True
-
-        reverse = str(x)[::-1]
-        return str(x)==reverse
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string = str(x)
+        return string == string[::-1]
