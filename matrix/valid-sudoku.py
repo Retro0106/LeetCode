17461f83beb0929e93a186ee679e8782262f270c
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-14 16:33:14 -0500

    LC-valid-sudoku [Runtime: 80 ms; Memory: 17.8 MB]

--- matrix/valid-sudoku.py
+++ matrix/valid-sudoku.py
@@ -2,33 +2,33 @@ from collections import Counter
 import copy
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # def checker(matrix):
-        #     for i in range(len(matrix)):
-        #         hashmap = Counter(matrix)
-        #         for key in hashmap:
-        #             if hashmap[key] > 1 and key != '.':
-        #                 return False
-        #     return True
+        def checker(matrix):
+            for i in range(len(matrix)):
+                hashmap = Counter(matrix)
+                for key in hashmap:
+                    if hashmap[key] > 1 and key != '.':
+                        return False
+            return True
         
-        # for i in range(len(board)):
-        #     if not checker(board[i]):
-        #         return False
+        for i in range(len(board)):
+            if not checker(board[i]):
+                return False
 
-        # board2 = copy.deepcopy(board)
-        # board3 = [[row[i] for row in board2] for i in range(len(board2[0]))]
+        board2 = copy.deepcopy(board)
+        board3 = [[row[i] for row in board2] for i in range(len(board2[0]))]
 
-        # for i in range(len(board3)):
-        #     if not checker(board3[i]):
-        #         return False
+        for i in range(len(board3)):
+            if not checker(board3[i]):
+                return False
         
-        # for i in range(0, len(board), 3):
-        #     for j in range(0, len(board[0]), 3):
-        #         arr = []
-        #         for x in range(3):
-        #             for y in range(3):
-        #                 arr.append(board[i + x][j + y])
-        #         if not checker(arr):
-        #             return False
+        for i in range(0, len(board), 3):
+            for j in range(0, len(board[0]), 3):
+                arr = []
+                for x in range(3):
+                    for y in range(3):
+                        arr.append(board[i + x][j + y])
+                if not checker(arr):
+                    return False
 
-        # return True
+        return True
         
