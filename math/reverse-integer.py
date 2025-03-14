568da87e5bedc3b29f2cbf132b9c51170949beab
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-14 17:32:26 -0500

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.py
+++ math/reverse-integer.py
@@ -10,6 +10,6 @@ class Solution:
             integer = round(integer // 10)
             reverse = reverse * 10 + curr
         
-        if x > 2**31-1 or reverse < -2**31-1:
+        if x > (2**31)-1 or x < (-2**31)-1:
             return 0
         return -reverse if negative else reverse
