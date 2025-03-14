57f58b6d2e08b8c8bd6bb87c98ba12b9d552801f
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-03-14 17:19:27 -0500

    LC-palindrome-number [Runtime: 10 ms; Memory: 17.9 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,4 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        string = str(x)
-        return string == string[::-1]
+        # string = str(x)
+        # return string == string[::-1]
+        if x < 0:
+            return False
+        integer = x
+        backwards = 0
+        while integer > 0:
+            curr = integer % 10
+            integer = round(integer // 10)
+            backwards = backwards * 10 + curr
+        return backwards == x
+            
