52c32de8ba5a5d6cb27cba97a2ef873242fb5de5
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-18 14:27:14 -0700

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -3,25 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i,j = 0,0
-        for j in range(len(nums)):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-            j += 1
-
-            
-        
-
-        
-        # Doesnt keep the order intact
-
         # i = 0
         # j = len(nums)-1
-        # while nums[j] == 0 and j>=0:
+        # while nums[j] == 0 and j > 0:
         #     j-=1
+        # if j <= 0:
+        #     return nums
         # while i < j:
         #     if nums[i] == 0:
         #         nums[i], nums[j] = nums[j], nums[i]
         #         j -= 1
-        #     i+=1
+        #     i += 1
+        # return nums
+        i = 0
+        while nums[i] != 0 and i < len(nums):
+            i += 1
+        if i == len(nums):
+            return nums
+        
+        for j in range(i, len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return nums
