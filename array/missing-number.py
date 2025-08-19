7b595bf8ee0ca4b7d1f34a094af3c44075717fbd
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-19 08:52:43 -0700

    LC-missing-number [Runtime: N/A; Memory: N/A]

--- array/missing-number.py
+++ array/missing-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
+        if len(nums) == 1:
+            if nums[0] == 0:
+                return 1
+            else: 
+                return 0
         for i in range(len(nums)-1):
             if nums[i]+1 != nums[i+1]:
                 return nums[i]+1
