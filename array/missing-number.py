951cc1f4e94d5780b39a5efbd43f41dc161bdaee
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-19 10:36:25 -0700

    LC-missing-number [Runtime: 0 ms; Memory: 19 MB]

--- array/missing-number.py
+++ array/missing-number.py
@@ -1,9 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        hashmap = {}
+        # hashmap = {}
+        # for num in nums:
+        #     hashmap[num] = 1
+        # for i in range(len(nums)):
+        #     if i not in hashmap:
+        #         return i
+        # return len(nums)
+        n = len(nums)
+        total = (n+1)*n//2
         for num in nums:
-            hashmap[num] = 1
-        for i in range(len(nums)):
-            if i not in hashmap:
-                return i
-        return len(nums)
+            total -= num
+        return total
