8552f2caec6b9714a8d43e833d8acf842e60e52e
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-18 22:42:21 -0700

    LC-remove-nth-node-from-end-of-list [Runtime: 0 ms; Memory: 17.9 MB]

--- linked-list/remove-nth-node-from-end-of-list.py
+++ linked-list/remove-nth-node-from-end-of-list.py
@@ -1,25 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        slow = fast = prev = head
-        if not head.next:
-            return None
-        
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast = head
         for i in range(n):
+            if not fast.next:
+                return head.next
             fast = fast.next
-        
-        while fast and fast.next:
-            fast = fast.next
+        while fast.next:
             slow = slow.next
-        
+            fast = fast.next
         slow.next = slow.next.next
-        return prev
+        return head
+
+        
