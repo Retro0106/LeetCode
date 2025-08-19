2bc79a83f98fb459b99491f3cb357b3d6d56d08a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-19 08:58:11 -0700

    LC-missing-number [Runtime: 5 ms; Memory: 19.1 MB]

--- hash-table/missing-number.py
+++ hash-table/missing-number.py
@@ -1,15 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        if 0 not in nums:
-            return 0
-        if len(nums) == 1:
-            if nums[0] == 0:
-                return 1
-            else: 
-                return 0
-        for i in range(len(nums)-1):
-            if nums[i]+1 != nums[i+1]:
-                return nums[i]+1
-        return nums[i+1]+1
-    
+        hashmap = {}
+        for num in nums:
+            hashmap[num] = 1
+        for i in range(len(nums)):
+            if i not in hashmap:
+                return i
+        return len(nums)
