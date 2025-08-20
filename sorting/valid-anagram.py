c21c479841c2c08a6c896e70b081e7864ded4ab0
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-20 12:26:07 -0700

    LC-valid-anagram [Runtime: 7 ms; Memory: 17.8 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,23 +1,6 @@
-from collections import Counter, defaultdict
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # s_Counter = Counter(s)
-        # t_Counter = Counter(t)
-        # return s_Counter == t_Counter
-
-        # hashmap = defaultdict(int)
-        # for char in s:
-        #     hashmap[char] += 1
-        # for char in t:
-        #     hashmap[char] -= 1
-        #     if hashmap[char] == -1:
-        #         return False
-        # return max(hashmap.values()) == 0
-
-        if len(s) != len(t):
-            return False
-        count = [0]*26
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')]+=1
-            count[ord(t[i]) - ord('a')]-=1
-        return all(c==0 for c in count)
+        hashmap = Counter(s)
+        hashmap2 = Counter(t)
+        return hashmap == hashmap2
