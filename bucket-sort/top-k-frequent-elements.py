80f64bf6b0751f77bd6d2467a6fb5e20f25af669
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-20 18:54:35 -0700

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- bucket-sort/top-k-frequent-elements.py
+++ bucket-sort/top-k-frequent-elements.py
@@ -12,7 +12,7 @@ class Solution:
         result = []
         count = 0
         for i in range(len(bucket)-1, -1, -1):
-            if bucket[i]:
+            if bucket[i] is not None:
                 result.append(bucket[i])
                 k -= 1
             if k == 0:
