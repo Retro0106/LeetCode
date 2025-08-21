bcbf7b2765874fce0f95bacf5b807368809db514
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-21 12:26:31 -0700

    LC-product-of-array-except-self [Runtime: 30 ms; Memory: 26 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -1,75 +1,16 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        
-        l_array = [1 for i in nums]
-        r_array = [1 for i in nums]
-        l_multiplier = r_multiplier = 1
-        i = 0
-        j = len(nums)-1
-        while i < len(nums):
-            l_array[i]=l_multiplier
-            r_array[j]=r_multiplier
-            l_multiplier*=nums[i]
-            r_multiplier*=nums[j]
-            i+=1
-            j-=1
-        answer = list(map(lambda x,y:x*y,l_array,r_array))
-        return answer
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # answer = [] 
-        # i = j = 0 
-        # mult = 1
-        # mult_with_zero = 1   
-        # for i in nums:
-        #     if i!= 0:
-        #         mult*=i
-        #     mult_with_zero *=i
-        # for i in nums:
-        #     if i!= 0:
-        #         if 0 in nums:
-        #             answer.append(0)
-        #         else:
-        #             answer.append(mult//i)
-        #     else:
-        #         if max(nums)==0:
-        #             answer.append(0)
-        #         elif nums.count(i)>1:
-        #             answer.append(mult_with_zero)
-        #         else:
-        #             answer.append(mult)
-        # return answer
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        lmult = 1
+        rmult = 1
 
-
-
-
-
-
-
-        # while i < len(nums) and j < len(nums):
-        #     if j == len(nums) -1:
-        #         if i != j:
-        #             mult*=nums[j]
-        #         j=0
-        #         i+=1
-        #         answer.append(mult)
-        #         mult = 1
-        #     else:
-        #         if i != j:
-        #             mult*=nums[j]
-        #         j+=1     
-        # return answer
-                
+        for i in range(len(nums)):
+            left[i] = lmult
+            lmult *= nums[i]
+        
+        for i in range(len(nums)-1, -1, -1):
+            right[i] = rmult
+            rmult *= nums[i]
+        
+        return list(map(lambda x,y: x*y, left, right))
