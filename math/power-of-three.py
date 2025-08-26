4880a1e6c4381d24105c046c907d0d365a6be438
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-26 12:50:04 -0700

    LC-power-of-three [Runtime: 0 ms; Memory: 17.8 MB]

--- math/power-of-three.py
+++ math/power-of-three.py
@@ -4,5 +4,5 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n <= 0:
             return False
-        power = int(math.log(n,3))
+        power = math.ceil((math.log(n,3)))
         return n == 3**power
