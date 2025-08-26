3dc6e55400acec7070d0ca5c6acf803f66224304
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-08-26 12:30:47 -0700

    LC-maximum-area-of-longest-diagonal-rectangle [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-area-of-longest-diagonal-rectangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        longest_diagonal = 0
+        maximum_area = 0
+        
+        for dimension in dimensions:
+            a,b = dimension
+            diagonal = a**2 + b**2
+            if diagonal > longest_diagonal:
+                maximum_area = a*b
+            elif diagonal == longest_diagonal:
+                maximum_area = max(maximum_area, a*b)
+        return maximum_area
