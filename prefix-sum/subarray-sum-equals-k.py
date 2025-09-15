05bb2edbcad6242cd19de836bd1bdce4d70bd5be
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-09-15 16:49:24 -0500

    LC-subarray-sum-equals-k [Runtime: 43 ms; Memory: 20.4 MB]

--- prefix-sum/subarray-sum-equals-k.py
+++ prefix-sum/subarray-sum-equals-k.py
@@ -1,29 +1,68 @@
+from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        left = 0
-        right = 0
-        curr = nums[left]
+        hashmap = defaultdict(int)
         count = 0
-
-        if len(nums) == 1:
-            if nums[0] == k:
-                return 1
-            else:
-                return 0
-
-        while right < len(nums):
-            while curr > k:
-                curr -= nums[left]
-                left += 1
-
-            if curr == k:
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            if total == k:
                 count += 1
-                left += 1
-                right += 1
-            else:
-                right += 1
-                if right != len(nums):
-                    curr += nums[right]
+            if total-k in hashmap:
+                count += hashmap[total-k]
+            hashmap[total]+=1
+            
 
-
+        
         return count
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # left = 0
+        # right = 0
+        # curr = nums[left]
+        # count = 0
+
+        # if len(nums) == 1:
+        #     if nums[0] == k:
+        #         return 1
+        #     else:
+        #         return 0
+
+        # while right < len(nums):
+        #     while curr != k:
+        #         curr -= nums[left]
+        #         left += 1
+
+        #     if curr == k:
+        #         count += 1
+        #         left += 1
+        #         right += 1
+        #     else:
+        #         right += 1
+        #         if right != len(nums):
+        #             curr += nums[right]
+
+
+        # return count
