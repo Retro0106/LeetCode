a725c1d28adf497e4fb9756253092103285ebe1b
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-10-01 15:51:03 -0500

    LC-subsets [Runtime: 0 ms; Memory: 17.9 MB]

--- array/subsets.py
+++ array/subsets.py
@@ -1,17 +1,63 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        result = []
         curr = []
+        n = len(nums)
+        
+        def backtracking(i):
+                if i == len(nums):
+                    result.append(curr[:])
+                    return
+                
+                curr.append(nums[i])
+                backtracking(i+1)
 
-        def dfs(i):
-            if i == len(nums):
-                res.append(curr[:])
-                return
+                curr.pop()
+                backtracking(i+1)
+
+        backtracking(0)
+        return result
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        # res = []
+        # curr = []
+
+        # def dfs(i):
+        #     if i == len(nums):
+        #         res.append(curr[:])
+        #         return
             
-            curr.append(nums[i])
-            dfs(i+1)
-            curr.pop()
+        #     curr.append(nums[i])
+        #     dfs(i+1)
+        #     curr.pop()
 
-            dfs(i+1)
-        dfs(0)
-        return res
+        #     dfs(i+1)
+        # dfs(0)
+        # return res
