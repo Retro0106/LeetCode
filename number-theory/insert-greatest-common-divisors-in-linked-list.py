d0545e603d3ee07930855462ffcd4bde1ed2fdfe
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-11 11:52:23 -0600

    LC-insert-greatest-common-divisors-in-linked-list [Runtime: 21 ms; Memory: 20.1 MB]

--- number-theory/insert-greatest-common-divisors-in-linked-list.py
+++ number-theory/insert-greatest-common-divisors-in-linked-list.py
@@ -6,14 +6,17 @@
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         def gcd(num1, num2):
-            minimum = min(num1, num2)
-            maximum = max(num1, num2)
-            # for num in range(minimum, 0, -1):
-            #     if maximum % num == 0 and minimum % num == 0:
-            #         return num
-            remainder = maximum % minimum
-            if remainder == 0: return minimum
-            return gcd(minimum, remainder)
+            # minimum = min(num1, num2)
+            # maximum = max(num1, num2)
+            # # for num in range(minimum, 0, -1):
+            # #     if maximum % num == 0 and minimum % num == 0:
+            # #         return num
+            # remainder = maximum % minimum
+            # if remainder == 0: return minimum
+            # return gcd(minimum, remainder)
+            while num2 > 0:
+                num1, num2 = num2, num1 % num2
+            return num1
         
         curr = head
         while curr and curr.next:
