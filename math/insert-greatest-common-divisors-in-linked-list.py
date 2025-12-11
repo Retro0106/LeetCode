25358777ae6999a2cf4956d951bf390b6f8e7c79
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-11 11:47:40 -0600

    LC-insert-greatest-common-divisors-in-linked-list [Runtime: 43 ms; Memory: 20.1 MB]

--- math/insert-greatest-common-divisors-in-linked-list.py
+++ math/insert-greatest-common-divisors-in-linked-list.py
@@ -8,9 +8,12 @@ class Solution:
         def gcd(num1, num2):
             minimum = min(num1, num2)
             maximum = max(num1, num2)
-            for num in range(minimum, 0, -1):
-                if maximum % num == 0 and minimum % num == 0:
-                    return num
+            # for num in range(minimum, 0, -1):
+            #     if maximum % num == 0 and minimum % num == 0:
+            #         return num
+            remainder = maximum % minimum
+            if remainder == 0: return minimum
+            return gcd(minimum, remainder)
         
         curr = head
         while curr and curr.next:
