25711842a4cdb8dd58ed4f7bdd9a31bbd1aeb32b
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-11 22:36:20 -0600

    LC-path-sum [Runtime: N/A; Memory: N/A]

--- binary-tree/path-sum.py
+++ binary-tree/path-sum.py
@@ -6,16 +6,52 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        
         def dfs(node, curr):
             if not node:
                 return False
+            if node.left == None and node.right == None:
+                return curr + node.val == targetSum
             
             curr += node.val
-            if not node.left and not node.right:
-                return curr == targetSum
-            
+            if curr > targetSum:
+                return False
+            if curr == targetSum:
+                return True
             return dfs(node.left, curr) or dfs(node.right, curr)
+        
+        return dfs(root, 0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # def dfs(node, curr):
+        #     if not node:
+        #         return False
+            
+        #     curr += node.val
+        #     if not node.left and not node.right:
+        #         return curr == targetSum
+            
+        #     return dfs(node.left, curr) or dfs(node.right, curr)
                     
             
         
-        return dfs(root, 0)
+        # return dfs(root, 0)
