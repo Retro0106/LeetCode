48ee57460d64b3f2034a5b4f792ffba61d316909
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-18 11:34:32 -0600

    LC-diameter-of-binary-tree [Runtime: 10 ms; Memory: 20.2 MB]

--- binary-tree/diameter-of-binary-tree.py
+++ binary-tree/diameter-of-binary-tree.py
@@ -6,22 +6,57 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_diameter = 0
         
+        # get no of nodes of left
+        # get no of nodes of right
+        # keep track of max diameter(left + right)
+        # return max(left, right)
+        maximum = 0
         def dfs(node):
+            nonlocal maximum
             if not node:
                 return 0
-
-            height_left = dfs(node.left)
-            height_right = dfs(node.right)
-
-            nonlocal max_diameter
-            max_diameter = max(max_diameter, height_left + height_right)
-
-            return 1 + max(height_left, height_right)
+            left = dfs(node.left)
+            right = dfs(node.right)
+            diameter = left + right
+            maximum = max(maximum, diameter)
+            return 1 + max(left, right)
         
         dfs(root)
-        return max_diameter
+        return maximum
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # max_diameter = 0
+        
+        # def dfs(node):
+        #     if not node:
+        #         return 0
+
+        #     height_left = dfs(node.left)
+        #     height_right = dfs(node.right)
+
+        #     nonlocal max_diameter
+        #     max_diameter = max(max_diameter, height_left + height_right)
+
+        #     return 1 + max(height_left, height_right)
+        
+        # dfs(root)
+        # return max_diameter
 
     
     
