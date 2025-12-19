b71a1ba15c898688868e685404aaba6a7d3d4fd0
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-18 18:57:32 -0600

    LC-binary-tree-right-side-view [Runtime: 0 ms; Memory: 17.4 MB]

---
+++ binary-tree/binary-tree-right-side-view.py
@@ -0,0 +1,26 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = deque([root])
+        res = []
+
+        while queue:
+            length = len(queue)
+
+            for i in range(length):
+                node = queue.popleft()
+                if i == 0:
+                    res.append(node.val)
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+        return res
