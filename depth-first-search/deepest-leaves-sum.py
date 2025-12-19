b1cd64901524eca2e8143edd554e7bd486109bdf
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-18 19:40:35 -0600

    LC-deepest-leaves-sum [Runtime: 15 ms; Memory: 20 MB]

---
+++ depth-first-search/deepest-leaves-sum.py
@@ -0,0 +1,27 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        queue = deque([root])
+        while queue:
+            length = len(queue)
+            total = 0
+            
+            for _ in range(length):
+                node = queue.popleft()
+                total += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+        return total
