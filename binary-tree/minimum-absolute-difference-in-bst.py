2bb215e010b9b75763279a2e50de3718923c3e57
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-27 21:04:58 -0600

    LC-minimum-absolute-difference-in-bst [Runtime: 3 ms; Memory: 18.7 MB]

--- binary-tree/minimum-absolute-difference-in-bst.py
+++ binary-tree/minimum-absolute-difference-in-bst.py
@@ -9,18 +9,50 @@ class Solution:
         # if leaf node: return infinity
         # each node would return ints MAD
         # for each root, return minimum of node.left MAD, node.right MAD, abs(node-node.left), abs(node-node.right)
-        
-        if not root:
-            return float('inf')
-        
-        if not root.left and not root.right:
-            return float('inf')
-        left = right = float('inf')
-        if root.left:
-            left = root.left.val
-        
-        if root.right:
-            right = root.right.val
+        self.arr = []
 
-        return min(self.getMinimumDifference(root.left), abs(root.val-left), abs(root.val-right),self.getMinimumDifference(root.right))
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            self.arr.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        minimum = float('inf')
+        if len(self.arr) < 2:
+            return 0
+        for i in range(1, len(self.arr)):
+            minimum = min(minimum, self.arr[i]-self.arr[i-1])
+        return minimum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not root:
+        #     return float('inf')
+        
+        # if not root.left and not root.right:
+        #     return float('inf')
+        # left = right = float('inf')
+        # if root.left:
+        #     left = root.left.val
+        
+        # if root.right:
+        #     right = root.right.val
+
+        # return min(self.getMinimumDifference(root.left), abs(root.val-left), abs(root.val-right),self.getMinimumDifference(root.right))
         
