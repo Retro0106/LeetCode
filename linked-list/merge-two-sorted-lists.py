30d98249f32aa8cce2a04b2185a10d0859154cff
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2025-12-30 09:12:46 -0600

    LC-merge-two-sorted-lists [Runtime: 3 ms; Memory: 17.2 MB]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -1,37 +1,26 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        head = ListNode()
-        curr = head
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                curr = curr.next
-                list1 = list1.next
-            
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        l1 = list1
+        l2 = list2
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
             else:
-                curr.next = list2
-                curr = curr.next
-                list2 = list2.next
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
         
-        if list1:
-            curr.next = list1
-        elif list2:
-            curr.next = list2
-
-        return head.next
-
-
-       
-
-        
-        
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
+        return dummy.next
