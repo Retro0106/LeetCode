2754be1a21a78161ac28712296c9dbd5c7eb1dff
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-02-02 21:26:40 -0600

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,26 +1,19 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s)-1
-        if len(s)<=1:
-            return True
+
         while left < right:
-            if not s[left].isalnum():
-                left+=1
-                continue
+            while left < right and not s[left].isalpha():
+                left += 1
+            while left < right and not s[right].isalpha():
+                right -= 1
+            if left < right:
+                if s[left].lower() != s[right].lower():
+                    return False
+            else:
+                return True
             
-            if not s[right].isalnum():
-                right-=1
-                continue
-            
-            if s[left].lower() != s[right].lower():
-                return False
-            
-            left+=1
-            right-=1
+            left += 1
+            right -= 1
         return True
-        
