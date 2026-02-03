24a3a097983b3c683ead814f7b1661ec8d9fb8d2
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-02-02 21:40:47 -0600

    LC-valid-parentheses [Runtime: 0 ms; Memory: 19.4 MB]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,5 +1,49 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        hashmap = {'(':')','[':']','{':'}'}
+
+        stack = []
+        for char in s:
+            if not stack:
+                if char not in hashmap:
+                    return False
+                stack.append(char)
+            else:
+                if char not in hashmap:
+                    if hashmap[stack[-1]] != char:
+                        return False
+                    else:
+                        stack.pop()
+                else:
+                    stack.append(char)
+        return len(stack)==0
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         stack = []
         hashmap = {'(':')', '{':'}', '[':']'}
 
