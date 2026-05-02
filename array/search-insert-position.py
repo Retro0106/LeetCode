5e8e73c31a62cc4d5cf2ac086296b50b7d4d547d
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-05-01 20:49:56 -0500

    LC-search-insert-position [Runtime: 0 ms; Memory: 19.9 MB]

--- array/search-insert-position.py
+++ array/search-insert-position.py
@@ -1,14 +1,20 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        i = 0
-        while i < len(nums):
-            if nums[i] < target:
-                i+=1
-            else:
-                return i
-        return i
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # initalize left, and right
+        # while loop checking while left <= right
+        # get mid
+        # if mid is less, index = mid + 1, left = mid + 1
+        # else, index = mid, right = mid - 1
+        left, right = 0, len(nums)-1
+        while left <= right: 
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                index  = mid + 1
+                left = mid + 1
+            if nums[mid] > target:
+                index = mid
+                right = mid - 1
+        return index
+                
