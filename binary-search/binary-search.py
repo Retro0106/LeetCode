bda11238a7e909d2b07a12f74080780c9daa954d
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-05-01 20:34:19 -0500

    LC-binary-search [Runtime: 0 ms; Memory: 20.5 MB]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -9,7 +9,7 @@ class Solution:
         left = 0
         right = len(nums)-1
 
-        while left < right:
+        while left <= right:
             mid = (left+right) // 2
             if nums[mid] == target:
                 return mid
