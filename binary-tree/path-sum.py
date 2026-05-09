f85ef0d022e172b9202b168f87b4f4b97395e60a
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-05-08 22:29:17 -0500

    LC-path-sum [Runtime: N/A; Memory: N/A]

--- binary-tree/path-sum.py
+++ binary-tree/path-sum.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,38 +7,39 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # if not root: return false
+        # initialize queue with (0, root node)
+        # while queue:
+            # for _ in range of len of queue
+                # pop curr sum and node
+                # update curr sum
+                # if node is a leaf, check if curr sum  == target Sum. if so return True
+                # if node.left: add to queue as (curr_sum, node.left)
+                # if node.right: add to queue as (curr_sum, node.right)
+        # return False
+
+        if not root: return False
+        queue = deque([(0, root)])
+        while queue:
+            for _ in range(len(queue)):
+                curr_sum, node = queue.popleft()
+                curr_sum += node.val
+                if not node.left and not node.right:
+                    if curr_sum == targetSum:
+                        return True
+                if node.left: queue.append((curr_sum, node.left))
+                if node.right: queue.append((curr_sum, node.right))
+        return False
+
         
-        
-        def dfs(node, curr):
-            if not node:
-                return False
-            if node.left == None and node.right == None:
-                return curr + node.val == targetSum
-            curr += node.val
-            
-            return dfs(node.left, curr) or dfs(node.right, curr)
-        
-        return dfs(root, 0)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
         # def dfs(node, curr):
         #     if not node:
         #         return False
