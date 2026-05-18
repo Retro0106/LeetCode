de16263ea6a0efba26a70a451ffe6067619a226e
Jermaine Adesanya <jermaineadesanya06@gmail.com>
2026-05-18 01:16:53 -0500

    LC-same-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/same-tree.py
+++ binary-tree/same-tree.py
@@ -8,6 +8,25 @@ from collections import deque
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1:
+                return False
+            if not node2:
+                return False
+
+            if node1.val != node2.val:
+                return False
+            
+
+            left = dfs(node1.left, node2.left)
+            right = dfs(node1.right, node2.right)
+
+            return left and right
+
+        return dfs(p,q)
+
         # initialize deque, and result list with root p and q
         # while queue,
             # initialize curr = []
@@ -15,67 +34,49 @@ class Solution:
                 # pop from queue, add to curr
         # compare that theyre both empty
 
-        queue_p = deque([p])
-        queue_q = deque([q])
+        # queue_p = deque([p])
+        # queue_q = deque([q])
 
-        while queue_p and queue_q:
-            n1 = queue_p.popleft()
-            n2 = queue_q.popleft()
+        # while queue_p and queue_q:
+        #     n1 = queue_p.popleft()
+        #     n2 = queue_q.popleft()
 
-            if not n1 and not n2:
-                continue
+        #     if not n1 and not n2:
+        #         continue
 
-            if not n1 or not n2 or n1.val != n2.val:
-                return False
-
-            queue_p.append(n1.left)
-            queue_p.append(n1.right)
-
-            queue_q.append(n2.left)
-            queue_q.append(n2.right)
-
-        return not queue_p and not queue_q
-
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-        # def dfs(node1, node2):
-        #     if not node1 and not node2:
-        #         return True
-        #     if not node1:
-        #         return False
-        #     if not node2:
+        #     if not n1 or not n2 or n1.val != n2.val:
         #         return False
 
-        #     if node1.val != node2.val:
-        #         return False
-            
+        #     queue_p.append(n1.left)
+        #     queue_p.append(n1.right)
 
-        #     left = dfs(node1.left, node2.left)
-        #     right = dfs(node1.right, node2.right)
+        #     queue_q.append(n2.left)
+        #     queue_q.append(n2.right)
 
-        #     return left and right
+        # return not queue_p and not queue_q
 
-        # return dfs(p,q)
-
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
         
 
         
